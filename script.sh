3e2990f4918a69fe748e1751168944b09a41ff6b
mohitbhagwani94 <mohitbhagwani94@gmail.com>
2017-01-17 00:08:05 +0530

    Enter your Message

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "============Start of Pull command============"
+git pull
+echo "============End of Pull command============"
+
+
+echo "============Start of Commit command============"
+git add .
+#git reset script.sh 
+git commit -m "Enter your Message"
+echo "============End of Commit command============"
+
+
+echo "============Start of Push command============"
+git push
+echo "============End of Push command============"
+
+
